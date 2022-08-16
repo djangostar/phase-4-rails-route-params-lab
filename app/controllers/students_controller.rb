a6fa339b6f605dbb333b2ad609c7ff907fcc678d
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-08-16 12:24:41 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
+require 'pry'
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # binding.pry
+    students = if params[:name]
+                Student.by_name(params[:name])
+               else 
+                Student.all
+               end
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
